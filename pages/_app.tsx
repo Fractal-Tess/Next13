f980678c941fdf5f645b1661bd78cd593d328418
Fractal-Tess <vgfractal@gmail.com>
2022-09-14 15:45:01 +0300

    what am i doing?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,49 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import NextApp, {
+  AppContext,
+  AppInitialProps,
+  AppProps as NextAppProps,
+} from 'next/app';
+import { defaultTheme, Theme, themes } from '$types';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { useState } from 'react';
+import cookie from 'cookie';
+import { ThemeContext } from '$context/ThemeToggle';
+import Header from 'components/core/Header';
+
+type AppProps = { cookieTheme: Theme | null };
+type CombineProps<T> = AppProps & T;
+
+export function App({
+  Component,
+  pageProps,
+  cookieTheme,
+}: CombineProps<NextAppProps>) {
+  const [theme, setTheme] = useState<Theme>(cookieTheme || defaultTheme);
+  return (
+    <>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <Header />
+        <Component {...pageProps} />
+      </ThemeContext.Provider>
+    </>
+  );
 }
 
-export default MyApp
+App.getInitialProps = async (
+  context: AppContext
+): Promise<CombineProps<AppInitialProps>> => {
+  const { theme } = cookie.parse(context.ctx.req?.headers.cookie || '') as {
+    theme: string | undefined;
+  };
+  let cookieTheme: Theme | null = null;
+  if (theme) {
+    if (themes.includes(theme as Theme)) {
+      cookieTheme = theme as Theme;
+    } else console.log(`Received invalid theme: ${theme}`);
+  }
+  const ctx = await NextApp.getInitialProps(context);
+  return { ...ctx, cookieTheme };
+};
+
+export default App;
