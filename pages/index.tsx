8a42cc2c2eeaaee6e0c523b683ac3c0bb5993aec
Fractal-Tess <vgfractal@gmail.com>
2022-09-14 23:46:51 +0300

    greeting text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
+import StyledSpan from '$components/text/styledSpan';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="flex flex-col items-center justify-center select-none space-y-4 text-5xl">
+      <h1 className="text-6xl font-bold">
+        Welcome to <StyledSpan text="Next.js" />
+      </h1>
+      <p>This is the Home page</p>
+    </div>
   );
 };
 
