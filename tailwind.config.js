c881b40c5e7ac00ad1fbc183252155bb4155b427
Fractal-Tess <vgfractal@gmail.com>
2022-09-14 12:47:09 +0300

    css updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
+const daisyui = require('daisyui');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+const config = {
+  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
-}
+
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: '#13dbde',
+          secondary: '#026773',
+          accent: '#F2E3D5',
+          neutral: '#024959',
+          'base-100': '#0a1930',
+          info: '#35B0F3',
+          success: '#1B743C',
+          warning: '#F38A12',
+          error: '#F2215C',
+        },
+      },
+    ],
+  },
+  plugins: [daisyui],
+};
+
+module.exports = config;
