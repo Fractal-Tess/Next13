a1c874d5337b0347b202767d940ed3d587b3b484
Fractal-Tess <vgfractal@gmail.com>
2022-09-14 23:45:54 +0300

    footer

---
+++ components/core/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <nav className="bg-base-100 h-12 shadow-top-xl flex justify-between px-8 items-center">
+      <div className="hover:text-primary"></div>
+    </nav>
+  );
+}
