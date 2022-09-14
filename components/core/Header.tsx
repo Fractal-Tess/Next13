5c9f49305d4c3d12c658033ea610287d63e1cb64
Fractal-Tess <vgfractal@gmail.com>
2022-09-14 23:46:00 +0300

    bg update

--- components/core/Header.tsx
+++ components/core/Header.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from '$components/buttons/ThemeToggle';
 
 export default function Header() {
   return (
-    <nav className="h-12 shadow-xl flex justify-between px-8 items-center">
+    <nav className="bg-base-100 h-12 shadow-2xl flex justify-between px-8 items-center">
       <div>{/* Logo */}</div>
 
       {/* Nav */}
